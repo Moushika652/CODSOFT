5295225f873d658044e615cc2514906110051c34
Moushika652 <mmoushika63@gmail.com>
2025-12-28 19:18:51 +0530

    initial

--- titanic_survival_project/test_root.py
+++
@@ -1,7 +0,0 @@
-from titanic_survival_project.webapp import app
-
-c = app.app.test_client()
-resp = c.get('/', follow_redirects=True)
-print('Final URL status:', resp.status_code)
-print('Final path snippet:', resp.request.path)
-print('Content length:', len(resp.data))
